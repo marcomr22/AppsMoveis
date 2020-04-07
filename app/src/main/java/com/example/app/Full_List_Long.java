c2391c039234b77014e999fce108be4383a13d3d
albos-iscteiul <albos@iscte-iul.pt>
2020-04-07 21:30:54 +0100

    test

---
+++ app/src/main/java/com/example/app/Full_List_Long.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Full_List_Long extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full__list__long);
+    }
+}
