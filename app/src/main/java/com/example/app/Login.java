cb507bbd63dc0cdb6dbcf211b4509a137033e507
jmcae11-iscteiul <jmcae11@iscte-iul.pt>
2020-04-07 21:52:53 +0100

    Activity Names Changed

--- app/src/main/java/com/example/app/Login.java
+++ app/src/main/java/com/example/app/Login.java
@@ -4,22 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         Button LoginButton = (Button) findViewById(R.id.Login);
 
@@ -35,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void registerButtonClick(View v){
-        Intent i = new Intent(MainActivity.this, Register.class);
+        Intent i = new Intent(Login.this, Register.class);
         startActivity(i);
     }
 
