6b2c04923ce5fd12e893cdf4949b41bd4475b335
albos-iscteiul <albos@iscte-iul.pt>
2020-04-08 13:07:52 +0100

    Menu Activity created

--- app/src/main/java/com/example/app/Menu.java
+++ app/src/main/java/com/example/app/Menu.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Full_List_Long extends AppCompatActivity {
+public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_full__list__long);
+        setContentView(R.layout.activity_menu);
     }
 }
