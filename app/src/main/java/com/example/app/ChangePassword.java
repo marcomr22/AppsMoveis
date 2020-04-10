cdae4bb4501ff1d0d74fa65493da433fa8f97d34
batocaa <pmaba1@iscte-iul.pt>
2020-04-10 20:01:28 +0100

    Added Sign Out and change password and fixed a bug in the login class.

--- app/src/main/java/com/example/app/ChangePassword.java
+++ app/src/main/java/com/example/app/ChangePassword.java
@@ -2,13 +2,40 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import authentication.FirebaseHandler;
 
 public class ChangePassword extends AppCompatActivity {
+    private EditText OldPassword;
+    private EditText NewPassword;
+    private EditText NewPassword2;
+    private Button ChangePasswordButton;
+    private FirebaseHandler firebaseHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+
+        OldPassword = findViewById(R.id.OldPassword);
+        NewPassword = findViewById(R.id.NewPassword);
+        NewPassword2 = findViewById(R.id.NewPassword2);
+        ChangePasswordButton = findViewById(R.id.ChangePasswordButton);
+
+        firebaseHandler = new FirebaseHandler();
+
+        ChangePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseHandler.ChangePassword(getApplicationContext(), OldPassword, NewPassword, NewPassword2);
+                Intent i = new Intent (ChangePassword.this, Menu.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/app/Menu.java
+++ app/src/main/java/com/example/app/Menu.java
@@ -2,13 +2,47 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import authentication.FirebaseHandler;
 
 public class Menu extends AppCompatActivity {
+    private Button ProfileSettingsButton;
+    private Button MyServicesButton;
+    private Button HiredServicesButton;
+    private Button LogOutButton;
+    private FirebaseHandler firebaseHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        ProfileSettingsButton = findViewById(R.id.button_profile_settings);
+        MyServicesButton = findViewById(R.id.MyServicesButton);
+        HiredServicesButton = findViewById(R.id.HiredServicesButton);
+        LogOutButton = findViewById(R.id.LogOutButton);
+
+        firebaseHandler = new FirebaseHandler();
+
+        ProfileSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Menu.this, ChangePassword.class);
+                startActivity(i);
+            }
+        });
+
+        LogOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseHandler.SignOut(getApplicationContext());
+                Intent i = new Intent(Menu.this, Login.class);
+                startActivity(i);
+            }
+        });
     }
 }
