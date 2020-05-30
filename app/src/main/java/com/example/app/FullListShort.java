8821926f0378815b9bbf2f7608d09025fd49f9ff
marcomr22 <37657498+marcomr22@users.noreply.github.com>
2020-05-30 16:49:36 +0100

    added exit onBackPressed

--- app/src/main/java/com/example/app/FullListShort.java
+++ app/src/main/java/com/example/app/FullListShort.java
@@ -131,4 +131,12 @@ public class FullListShort extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onBackPressed(){
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
+
 }
